3834c6b1a94f4874451ec312d7ef3e805109920e
schapira <or.schapira@gmail.com>
2015-01-20 07:52:06 +0200

    wikibot orginaize

--- wikitext/Boot/my-app/src/main/java/com/mycompany/app/App.java
+++ wikitext/Boot/my-app/src/main/java/com/mycompany/app/App.java
@@ -1,11 +1,16 @@
 package com.mycompany.app;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
 
 import net.sourceforge.jwbf.core.contentRep.Article;
 import net.sourceforge.jwbf.core.contentRep.SimpleArticle;
 import net.sourceforge.jwbf.mediawiki.actions.queries.AllPageTitles;
+import net.sourceforge.jwbf.mediawiki.actions.util.RedirectFilter;
 import net.sourceforge.jwbf.mediawiki.bots.MediaWikiBot;
 
 /**
@@ -15,30 +20,29 @@ import net.sourceforge.jwbf.mediawiki.bots.MediaWikiBot;
 public class App 
 {
     @SuppressWarnings({ "resource", "deprecation" })
-	public static void main( String[] args )
+	public static void main( String[] args ) throws FileNotFoundException, UnsupportedEncodingException
     {
     
         MediaWikiBot wikiBot = new MediaWikiBot("https://he.wikisource.org/w/");
-        Article article = wikiBot.getArticle("משתמשת:אור שפירא");
-        
-        //System.out.println(article.getText());
-        //System.out.print("password:");
+     //   Article article = wikiBot.getArticle("משתמשת:אור שפירא");     
+    //    System.out.println(article.getText());
+        System.out.print("password:");
         Scanner input = new Scanner(System.in);
         String pswd = input.nextLine();
-
-        AllPageTitles pages = new AllPageTitles(wikiBot);
-        Iterator<String> it = pages.iterator();
-         while (it.hasNext())
-        {
-        	System.out.println(it.next());
-        }
-        wikiBot.login("OrBoot", pswd);
-        
-       //wikiBot.writeContent(new SimpleArticle("לךגכחךדלגחכד", "משתמשת:אור שפירא/דגכחלידלגחכי"));
+       wikiBot.login("OrBoot", pswd);
         System.out.println("logged in");
+        String query="תוספתא/אוהלות";
+        Iterator<String> = searchList(wikiBot, query);
       //  article.save();
       }
+    private static Iterator<String> searchList(MediaWikiBot wikiBot, String query){
+    	RedirectFilter rf = RedirectFilter.nonredirects;
+    	 AllPageTitles pages = new AllPageTitles(wikiBot, null, query, rf);
+    	 pages.iterator();
+    	return pages.iterator();
 
+
+    }
       static void applyChangesTo(Article article) {
         // edits the article...
     	
